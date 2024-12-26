bb0edb9dce27482643e96402002106bb171be026
Dhiyanesh <dhiyaneshsasi03@gmail.com>
2024-12-26 13:07:36 +0530

    Cooler kid bug fix

--- frontend/src/components/ui/userListCard.jsx
+++ frontend/src/components/ui/userListCard.jsx
@@ -11,19 +11,24 @@ export function CardSpotlightDemo(props) {
     const isCurrentCoolestKid = currentUser.role === 'Coolest Kid';
     const isCurrentMaintainer = authService.isMaintainer();
 
+    // Determine if gradients should be applied
+    const applyGradients = isCurrentCoolestKid && isCurrentMaintainer;
+
+
+
     return (
-        <div className="relative">
-            <Card className={`relative rounded-2xl px-6 py-3 transform transition hover:scale-100 ${user.role === 'Cooler Kid' ? 'cooler-gradient-shadow' : 'bg-opacity-80'}  ${isCoolestKid ? 'coolest-gradient-shadow' : 'bg-opacity-80'}`}>
+        <div className="relative" >
+            <Card className={`relative rounded-2xl px-6 py-3 transform transition hover:scale-100 ${applyGradients ? (isCoolestKid ? 'coolest-gradient-shadow' : 'cooler-gradient-shadow') : 'bg-opacity-80'}`}>
                 <CardBody>
                     <div className="flex items-center mb-4">
-                        <div className="w-16 h-16 bg-dark-bg rounded-full flex items-center justify-center text-dark-text text-2xl mr-4">
+                        <div className="w-16 h-16 bg-dark-bg rounded-full flex items-center justify-center text-dark-text text-2xl mr-4" >
                             {user.firstName[0].toUpperCase()}
                         </div>
                         <div>
                             <h3 className="text-xl font-bold text-dark-text">
                                 {user.firstName} {user.lastName}
                             </h3>
-                            {isCurrentCoolestKid || isCurrentMaintainer && (
+                            {(isCurrentCoolestKid || isCurrentMaintainer) && (
                                 <p className="text-sm">{user.email}</p>
                             )}
                         </div>
@@ -33,7 +38,7 @@ export function CardSpotlightDemo(props) {
                             <FaGlobe className="mr-2 text-dark-text" />
                             <span className="font-medium text-dark-text">{user.country}</span>
                         </div>
-                        {isCurrentCoolestKid || isCurrentMaintainer && (
+                        {(isCurrentCoolestKid || isCurrentMaintainer) && (
                             <div className="flex items-center">
                                 <FaTag className="mr-2 text-dark-text" />
                                 <span className="font-medium">{user.role}</span>
